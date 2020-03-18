49c97c5b2c6edaf06900a4c097be8a0a8407da2b
Andrey Koleshko <ka8725@gmail.com>
2020-03-18 22:09:51 +0300

    Fixes

--- radar.rb
+++ radar.rb
@@ -1,77 +1,77 @@
 # frozen_string_literal: true
 
 class Radar
-  ALLOWED_POINTS = ['o', '-'].freeze
+  INVADER_POINTS = ['o', '-'].freeze
   NOISE = 'O'
 
   Point = Struct.new(:x, :y, keyword_init: true)
-  Invader = Struct.new(:lines, :width, :height, keyword_init: true)
-  Sample = Struct.new(:lines, :width, :height, keyword_init: true)
+  Sheet = Struct.new(:lines, :width, :height, keyword_init: true)
 
-  attr_accessor :invaders
+  attr_accessor :invaders, :sample
 
-  # @param invaders [Array<String>]
-  def initialize(invaders)
-    @invaders = invaders.compact.map { |invader| Invader.new(extract_display_data(invader)) }
+  # @option invaders [Array<String>]
+  # @option sample [String|nil] if nil, sets an empty sheet
+  def initialize(invaders:, sample:)
+    @invaders = invaders.compact.map { |invader| Sheet.new(extract_sheet(:invader, invader)) }
+    @sample = Sheet.new(extract_sheet(:sample, sample || ''))
   end
 
-  # @param sample [String]
+  # @option noise_threshold [Integer]
   # @return [Set<Point>]
-  def find_invaders(sample, allowed_noise: 0)
-    return Set.new if sample.nil?
-    sample_display = Sample.new(extract_display_data(sample, sample: true))
-    matched_invaders(invaders, sample_display, allowed_noise: allowed_noise)
+  def find_invaders(noise_threshold: 0)
+    matched_invaders(invaders, sample, noise_threshold: noise_threshold)
   end
 
   private
 
-  def matched_invaders(invaders, sample_display, allowed_noise:)
+  def matched_invaders(invaders, sample_display, noise_threshold:)
     invaders.each_with_object(Set.new) do |invader, result|
       max_x = sample_display.height - invader.height
       max_y = sample_display.width - invader.width
       (0..max_x).each do |x|
         (0..max_y).each do |y|
           point = Point.new(x: x, y: y)
-          result.add(point) if invader_matches?(sample_display, point, invader, allowed_noise: allowed_noise)
+          result.add(point) if invader_matches?(sample_display, point, invader, noise_threshold: noise_threshold)
         end
       end
     end
   end
 
-  def invader_matches?(sample_display, point, invader, allowed_noise:)
+  def invader_matches?(sample_display, point, invader, noise_threshold:)
     noise = 0
     (0..invader.height.pred).each do |x|
       (0..invader.width.pred).each do |y|
         xx = x + point.x
         yy = y + point.y
         noise += 1 if sample_display.lines[xx][yy] != invader.lines[x][y]
-        return false if noise > allowed_noise
+        return false if noise > noise_threshold
       end
     end
     true
   end
 
-  def extract_display_data(joined_line, sample: false)
-    lines = split_lines(joined_line, sample: sample)
+  # @param entity_type [Symbol] :invader or :sample
+  def extract_sheet(entity_type, joined_line)
+    lines = split_lines(entity_type, joined_line)
     {lines: lines, width: lines.first&.size || 0, height: lines.size}
   end
 
-  def split_lines(joined_line, sample:)
-    joined_line.split.tap do |res|
-      assert_allowed_points!(res, sample: sample)
-      assert_rectangle!(res)
+  def split_lines(entity_type, joined_line)
+    joined_line.split.tap do |lines|
+      assert_allowed_points!(entity_type, lines)
+      assert_rectangle!(entity_type, lines)
     end
   end
 
-  def assert_rectangle!(res)
-    raise ArgumentError, "sample or invader must be rectangular" if res.map(&:size).uniq.size > 1
+  def assert_rectangle!(entity_type, lines)
+    raise ArgumentError, "#{entity_type} must be rectangular" if lines.map(&:size).uniq.size > 1
   end
 
-  def assert_allowed_points!(lines, sample:)
-    allowed_points = sample ? ALLOWED_POINTS + [NOISE] : ALLOWED_POINTS
+  def assert_allowed_points!(entity_type, lines)
+    allowed_points = entity_type == :sample ? INVADER_POINTS + [NOISE] : INVADER_POINTS
     lines.each do |line|
       line.each_char do |char|
-        raise ArgumentError, "not allowed char: #{char}" unless allowed_points.include?(char)
+        raise ArgumentError, "not allowed char for #{entity_type}: #{char}" unless allowed_points.include?(char)
       end
     end
   end
--- spec/radar_spec.rb
+++ spec/radar_spec.rb
@@ -1,33 +1,80 @@
 # TODO: edge case invader greater than sample
 RSpec.describe Radar do
-  subject(:radar) { described_class.new(invaders) }
+  subject(:radar) { described_class.new(invaders: invaders, sample: sample) }
 
   let(:invaders) { ["--\noo"] }
+  let(:sample) { "--\noo" }
 
   def point(x, y)
     described_class::Point.new(x: x, y: y)
   end
 
-  def invader(lines)
-    described_class::Invader.new(lines: lines, width: lines.first.size, height: lines.size)
+  def sheet(lines)
+    described_class::Sheet.new(lines: lines, width: lines.first&.size || 0, height: lines.size)
   end
 
-  describe '#initialize invaders' do
-    subject { radar.invaders }
+  describe '#initialize' do
+    describe 'invaders:' do
+      subject { radar.invaders }
 
-    context 'when some invaders are nil' do
-      let(:invaders) { [nil, 'o'] }
-      it { is_expected.to eq([invader(['o'])]) }
+      context 'when some invaders are nil' do
+        let(:invaders) { [nil, 'o'] }
+        it { is_expected.to eq([sheet(['o'])]) }
+      end
+
+      context 'when is a multiline' do
+        let(:invaders) { ["o\n-"] }
+        it { is_expected.to eq([sheet(['o', '-'])]) }
+      end
+
+      context 'when has invalid points' do
+        let(:invaders) { ['x'] }
+        specify { expect { subject }.to raise_error(ArgumentError, 'not allowed char for invader: x') }
+      end
+
+      context 'when has noise points' do
+        let(:invaders) { ['O'] }
+        specify { expect { subject }.to raise_error(ArgumentError, 'not allowed char for invader: O') }
+      end
+
+      context 'when is not rectangular' do
+        let(:invaders) { ["-\n--"] }
+        specify { expect { subject }.to raise_error(ArgumentError, 'invader must be rectangular') }
+      end
     end
 
-    context 'when invader is a multiline' do
-      let(:invaders) { ["o\n-"] }
-      it { is_expected.to eq([invader(['o', '-'])]) }
+    describe 'sample:' do
+      subject { radar.sample }
+
+      context 'when is nil' do
+        let(:sample) { nil }
+        it { is_expected.to eq(sheet([])) }
+      end
+
+      context 'when is a multiline' do
+        let(:sample) { "o\n-" }
+        it { is_expected.to eq(sheet(['o', '-'])) }
+      end
+
+      context 'when has noise' do
+        let(:sample) { "o\nO" }
+        it { is_expected.to eq(sheet(['o', 'O'])) }
+      end
+
+      context 'when has invalid points' do
+        let(:sample) { 'x' }
+        specify { expect { subject }.to raise_error(ArgumentError, 'not allowed char for sample: x') }
+      end
+
+      context 'when is not rectangular' do
+        let(:sample) { "-\n--" }
+        specify { expect { subject }.to raise_error(ArgumentError, 'sample must be rectangular') }
+      end
     end
   end
 
   describe '#find_invaders' do
-    subject { radar.find_invaders(sample) }
+    subject { radar.find_invaders }
 
     context 'when sample is empty string' do
       let(:sample) { '' }
@@ -71,7 +118,7 @@ RSpec.describe Radar do
       it { is_expected.to eq([].to_set) }
 
       context 'when increased allowed noise' do
-        subject { radar.find_invaders(sample, allowed_noise: 8) }
+        subject { radar.find_invaders(noise_threshold: 8) }
 
         it { is_expected.to eq([point(13, 60), point(0, 42)].to_set) }
       end
